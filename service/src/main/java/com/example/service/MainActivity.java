7e1977f40ef489ac7291e7198089fc3873858910
liusiyuan1111 <liusiyuan1111@gmail.com>
2020-11-13 12:14:25 +0800

    3

--- service/src/main/java/com/example/service/MainActivity.java
+++ service/src/main/java/com/example/service/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnStart,btnStop;
+        btnStart = findViewById(R.id.start);
+        btnStop = findViewById(R.id.stop);
+        final Intent intent = new Intent(MainActivity.this,MyService.class);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //启动服务
+                startService(intent);
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //停止服务的方法2
+                stopService(intent);
+
+            }
+        });
     }
 }
---
+++ service/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,56 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {  //构造函数
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {  //绑定服务
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() { //创建服务
+        Log.i("服务状态：","服务已创建");
+        super.onCreate();
+    }
+    volatile boolean isStop = false;
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {  //启动服务
+        //创建新线程
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("服务状态：","服务已启动");
+                int i = 0;
+                while (!isStop){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ++i;
+                    Log.i("服务执行中：",Integer.toString(i));
+                }
+                //停止服务方法1
+                //stopSelf();
+
+            }
+        }).start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {   //销毁服务
+        isStop = true;
+        Log.i("服务状态：","服务已销毁");
+        super.onDestroy();
+    }
+}
